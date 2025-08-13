69323d3deadd8ea55df16d7cae32f1ea0ec25f73
MatheusMottaGit <matheusprofess@gmail.com>
2025-08-13 20:53:51 -0300

    calculando salario com irpf

--- appAula02/Program.cs
+++ appAula02/Program.cs
@@ -1,11 +1,36 @@
-﻿string nome;
-byte idade;
+﻿//string nome;
+//byte idade;
 
-Console.Write("digite seu nome: ");
-nome = Console.ReadLine();
+//Console.Write("digite seu nome: ");
+//nome = Console.ReadLine();
 
 
-Console.Write("digite sua idade: ");
-idade = Convert.ToByte(Console.ReadLine());
+//Console.Write("digite sua idade: ");
+//idade = Convert.ToByte(Console.ReadLine());
 
-Console.WriteLine(nome + idade);
+//Console.WriteLine(nome + idade);
+
+//idade *= 2;
+
+//Console.WriteLine($"Idade atual: {idade++}");
+
+
+// exercicio
+double salary;
+
+Console.Write("Digite seu salário: ");
+salary = Convert.ToDouble(Console.ReadLine());
+
+if (salary <= 1518)
+{
+    salary -= salary * 0.075;
+} else if (salary >= 1518 && salary <= 2793.88) {
+    salary -= salary * 0.095;
+} else if (salary >= 2793.88 && salary <= 4190.83) {
+    salary -= salary * 0.12;
+} else if (salary >= 4190.84 && salary <= 8157.41)
+{
+    salary -= salary * 0.14;
+}
+
+Console.WriteLine($"Salário com desconto: {salary}");
